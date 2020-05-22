4ea3bd5e445736dd8ce70a13ab5bff369309d1ec
MusicDev33 <smccowan33@gmail.com>
2020-05-22 14:51:32 -0600

    Routes set up for announcements

--- src/routes/announcements/route.collector.ts
+++ src/routes/announcements/route.collector.ts
@@ -1,1 +1,4 @@
 export * from './creation';
+export * from './destroy';
+export * from './getters';
+export * from './setters';
--- src/routes/announcements/routes.ts
+++ src/routes/announcements/routes.ts
@@ -3,7 +3,13 @@ const router = express.Router();
 import passport from 'passport';
 import * as RouteFunctions from './route.collector';
 
+router.get('/param/:param/:value', passport.authenticate('jwt', {session: false}), RouteFunctions.getAnnouncementsByParameterRoute);
+
 router.post('/create', passport.authenticate('jwt', {session: false}), RouteFunctions.createAnnouncementRoute);
+router.post('/onert/read', passport.authenticate('jwt', {session: false}), RouteFunctions.readOneAnnouncementRoute);
+router.post('/rts/read', passport.authenticate('jwt', {session: false}), RouteFunctions.readAnnouncementsRoute);
+
+router.delete(':id', passport.authenticate('jwt', {session: false}), RouteFunctions.deleteAnnouncementRoute);
 
 const AnnouncementRoutes = router;
 export default AnnouncementRoutes;
