05ded258e3bc8c873de3d1a963e760b33eab41c9
MusicDev33 <smccowan33@gmail.com>
2020-07-08 10:25:21 -0600

    default path

--- src/index.ts
+++ src/index.ts
@@ -138,6 +138,12 @@ app.get(apiBase, (req, res) => {
   res.status(404).send(resText + resImg);
 });
 
+app.get('', (req, res) => {
+  const resText = '<h1>404 - Here\'s a cool picture of Blaziken and Lucario:<br><br>';
+  const resImg = '<img src="https://pm1.narvii.com/6179/5434c40be48978d53a89c43c581bb0d84d1a4c56_hq.jpg">';
+  res.status(404).send(resText + resImg);
+});
+
 app.listen(port, () => {
   console.log('\nAstria Backend started in mode \'' + process.env.NODE_ENV + '\'');
   if (process.env.NODE_ENV === 'PRODUCTION' || process.env.NODE_ENV === 'DEVTEST') {
